67189144567f7273e584ae3fc4edbbbf05e1f059
ebarroso24 <134114010+ebarroso24@users.noreply.github.com>
2023-10-16 00:04:31 -0400

    implemented localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,10 @@ const cityInput = document.querySelector(".city-input");
 const searchButton = document.querySelector(".search-btn");
 const currentWeather = document.querySelector(".current-weather");
 const weatherCards = document.querySelector(".weather-cards");
-
+// delete below if it sucks
+const separator = document.querySelector(".separator");
+const storageKey = 'weatherSearchResults';
+// delete above if it sucks
 const createWeatherCard = (cityName, weatherItem, index) => {
 
 if (index ===0) {
@@ -30,6 +33,93 @@ return `<div class="details">
     
 };
 
+// delete below if it does not work
+// Function to save search result to local storage
+function saveToLocalStorage(city, data) {
+    let searchResults = JSON.parse(localStorage.getItem(storageKey)) || {};
+    searchResults[city] = data;
+    localStorage.setItem(storageKey, JSON.stringify(searchResults));
+}
+
+// Function to display search results from local storage
+function displaySearchResults() {
+    const searchResults = JSON.parse(localStorage.getItem(storageKey)) || {};
+    separator.innerHTML = ''; // Clear existing search results
+
+    for (const city in searchResults) {
+        const result = searchResults[city];
+
+        // Create a container for each result
+        const resultElement = document.createElement('div');
+        resultElement.classList.add('search-result');
+
+        // Display city name
+        const cityNameElement = document.createElement('strong');
+        cityNameElement.textContent = `${city}: `;
+        resultElement.appendChild(cityNameElement);
+
+        // Display weather description and temperature
+        const weatherInfoElement = document.createElement('span');
+        weatherInfoElement.textContent = `${result.weather[0].description}, ${result.main.temp}°C`;
+        resultElement.appendChild(weatherInfoElement);
+
+        separator.appendChild(resultElement);
+    }
+}
+
+// Function to handle the search button click
+// Function to fetch weather data from OpenWeatherMap API
+async function getWeather(city) {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching weather data:', error);
+        return null;
+    }
+}
+
+async function searchWeather() {
+    const cityName = cityInput.value.trim();
+
+    if (cityName === '') {
+        alert('Please enter a city name');
+        return;
+    }
+
+    const weatherData = await getWeather(cityName);
+
+    if (weatherData) {
+        // Save search result to local storage
+        saveToLocalStorage(cityName, weatherData);
+
+        // Display search results
+        displaySearchResults();
+
+        // Reset input value after search
+        cityInput.value = '';
+    } else {
+        alert('Failed to fetch weather data. Please try again.');
+    }
+}
+
+// Add an event listener to the search button
+searchButton.addEventListener('click', searchWeather);
+
+// Add an event listener to the city input for Enter key
+cityInput.addEventListener('keyup', (e) => {
+    if (e.key === 'Enter') {
+        searchWeather();
+    }
+});
+
+// Display stored search results when the page loads
+displaySearchResults();
+
+// delete above if it does not work
 const API_KEY = "190a3d5c8f4239c73d6b38d896670514";
 
 const getWeatherDetails = (cityName, lat, lon) => {
